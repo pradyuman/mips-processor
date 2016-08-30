af88b0043a943be272ff8db795ad79f1cd7c025e
Pradyuman Vig <pradyumanvig@outlook.com>
2016-08-30 01:24:17 -0400

    Update register file testbench

--- source/register_file.sv
+++ source/register_file.sv
@@ -1,26 +1,18 @@
-`include "cpu_types_pkg.vh"
 `include "register_file_if.vh"
 
-import cpu_types_pkg::*;
+module register_file (input logic CLK, nRST, register_file_if.rf rfif);
+   // 32 x 32 register
+   cpu_types_pkg::word_t [31:0] register_file;
 
-module register_file (
-  input logic CLK, nRST,
-  register_file_if.rf rfif
-);
+   // read logic
+   assign rfif.rdat1 = register_file[rfif.rsel1];
+   assign rfif.rdat2 = register_file[rfif.rsel2];
 
-// 32 x 32 register
-word_t [31:0] register_file;
-
-// read logic
-assign rfif.rdat1 = register_file[rfif.rsel1];
-assign rfif.rdat2 = register_file[rfif.rsel2];
-
-// write and reset logic
-always_ff @ (posedge CLK, negedge nRST) begin
-  if (nRST == 1'b0)
-    register_file <= '0;
-  else if (rfif.WEN && rfif.wsel != 0)
-    register_file[rfif.wsel] <= rfif.wdat;
-end
-
+   // write and reset logic
+   always_ff @ (posedge CLK, negedge nRST) begin
+      if (nRST == 1'b0)
+        register_file <= '0;
+      else if (rfif.WEN && rfif.wsel != 0)
+        register_file[rfif.wsel] <= rfif.wdat;
+   end
 endmodule
--- testbench/register_file_tb.sv
+++ testbench/register_file_tb.sv
@@ -1,48 +1,96 @@
-/*
-  Eric Villasenor
-  evillase@gmail.com
-
-  register file test bench
-*/
-
 // mapped needs this
 `include "register_file_if.vh"
+import cpu_types_pkg::word_t;
 
 // mapped timing needs this. 1ns is too fast
 `timescale 1 ns / 1 ns
 
 module register_file_tb;
+   parameter PERIOD = 10;
+   parameter RFILE_SIZE = 32;
+   logic CLK = 0, nRST;
 
-  parameter PERIOD = 10;
+   // test vars
+   int   v1 = 1;
+   int   v2 = 4721;
+   int   v3 = 25119;
 
-  logic CLK = 0, nRST;
+   // clock
+   always #(PERIOD/2) CLK++;
 
-  // test vars
-  int v1 = 1;
-  int v2 = 4721;
-  int v3 = 25119;
+   // interface
+   register_file_if rfif();
+   // DUT
+   register_file DUT(.CLK, .nRST, .rfif);
 
-  // clock
-  always #(PERIOD/2) CLK++;
+   initial begin
+      // $display("ns\tCLK\tnRST\tWEN");
+      // $monitor("%0.2d\t%b\t%b\t%b", $time, CLK, nRST, rfif.WEN);
 
-  // interface
-  register_file_if rfif ();
-  // DUT
-`ifndef MAPPED
-  register_file DUT(CLK, nRST, rfif);
-`else
-  register_file DUT(
-    .\rfif.rdat2 (rfif.rdat2),
-    .\rfif.rdat1 (rfif.rdat1),
-    .\rfif.wdat (rfif.wdat),
-    .\rfif.rsel2 (rfif.rsel2),
-    .\rfif.rsel1 (rfif.rsel1),
-    .\rfif.wsel (rfif.wsel),
-    .\rfif.WEN (rfif.WEN),
-    .\nRST (nRST),
-    .\CLK (CLK)
-  );
-`endif
+      // Error vector
+      static struct {
+         integer resetFile;
+         integer resetRead;
+      } errors = '{default:0};
 
-initial #100 $finish;
+      // Test nRST
+      $display("Testing Internal Register File on reset...");
+      reset();
+      testInternalFile('{default:0}, errors.resetFile);
+      if (!errors.resetFile) $display("SUCCESS: Internal Register File properly reset.");
+      else $display("FAILURE: %0d errors resetting Internal Register File.", errors.resetFile);
+
+      $display("Testing register file read after reset...");
+      testRead('{default:0}, errors.resetRead);
+      if (!errors.resetRead) $display("SUCCESS: Read working properly after reset.");
+      else $display("FAILURE: %0d errors reading from register file after reset", errors.resetRead);
+
+      rfif.WEN = 1'b1;
+      #PERIOD
+      for (int i=0; i<32; i++) begin
+         rfif.wsel = i;
+         rfif.wdat = i;
+         #PERIOD;
+      end
+
+      for (int i=0; i<32; i++)
+         $display("%0.2d: %0.2d", i, DUT.register_file[i]);
+      $finish;
+   end // initial begin
+
+   // Reset Register File
+   task reset;
+      nRST = 1'b1; #PERIOD;
+      nRST = 1'b0; #PERIOD;
+      nRST = 1'b1; #PERIOD;
+   endtask
+
+   // Test Internal Register Memory on Reset
+   task automatic testInternalFile(input word_t [RFILE_SIZE:0] testData, ref integer e);
+      foreach (testData[,i])
+         assert(DUT.register_file[i] == testData[i]) else begin
+            $display("ERROR: DUT.register_file[%0d] (%0d) != testData[%0d] (%0d)",
+                     i, DUT.register_file[i], i, testData[i]);
+            e++;
+         end
+   endtask
+
+   // Test RDAT
+   task automatic testRead(input word_t [RFILE_SIZE:0] testData, ref integer e);
+      foreach (testData[,i]) begin
+         rfif.rsel1 = i;
+         rfif.rsel2 = i;
+         #PERIOD
+         assert(rfif.rdat1 == testData[i]) else begin
+            $display("ERROR: rfif.rdat1[%0d] (%0d) != testData[%0d] (%0d)",
+                     i, rfif.rdat1, i, testData[i]);
+            e++;
+         end
+         assert(rfif.rdat2 == testData[i]) else begin
+            $display("ERROR: rfif.rdat2[%0d] (%0d) != testData[%0d] (%0d)",
+                     i, rfif.rdat2, i, testData[i]);
+            e++;
+         end
+      end
+   endtask
 endmodule
