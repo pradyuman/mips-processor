8bb1acefc076ed8cb4da139a8d3009f269184d85
Pradyuman Vig <pradyumanvig@outlook.com>
2016-09-23 03:46:08 -0400

    refactor asmFiles and testbenches

--- testbench/memory_control_tb.sv
+++ testbench/memory_control_tb.sv
@@ -1,95 +1,83 @@
-`include "cpu_types_pkg.vh"
 `include "cache_control_if.vh"
-`include "cpu_ram_if.vh"
 `include "caches_if.vh"
-`timescale 1ns / 1ns
+`include "cpu_ram_if.vh"
+`timescale 1 ns / 1 ns
+import cpu_types_pkg::word_t;
 
-import cpu_types_pkg::*;
-
-module memory_control_tb;
-
-  parameter PERIOD = 10;
+module memory_control_tb #(parameter PERIOD = 10);
   logic CLK = 0, nRST;
+  always #(PERIOD/2) CLK++;
 
-   always #(1) CLK++;
+  caches_if cif();
+  cache_control_if #(.CPUS(1)) ccif(.cif0(cif), .cif1(cif));
 
-  caches_if cif0 ();
-  caches_if cif1 ();
-  cache_control_if #(.CPUS(1)) ccif (cif0, cif1);
-  cpu_ram_if ramif ();
-
-  memory_control DUT_M (ccif);
-  ram DUT_R (CLK, nRST, ramif);
+  cpu_ram_if ramif();
+  ram RAM(.CLK, .nRST, .ramif);
+  memory_control DUT(.ccif);
 
   assign ccif.ramload = ramif.ramload;
   assign ccif.ramstate = ramif.ramstate;
-
+  assign ramif.ramstore = ccif.ramstore;
   assign ramif.ramaddr = ccif.ramaddr;
   assign ramif.ramWEN = ccif.ramWEN;
   assign ramif.ramREN = ccif.ramREN;
-  assign ramif.ramstore = ccif.ramstore;
 
-  initial
-  begin
-//111111111111111111111
-      #(1.5)
-      cif0.iaddr = 0;
-      cif0.iREN = 0;
-      cif0.dREN = 0;
-      cif0.dWEN = 0;
-      cif0.dstore = 0;
-      cif0.daddr = 0;
-      nRST = 1;
-      #(10)
-      nRST = 0;
-      #(10)
-      nRST = 1;
-      cif0.iaddr = 0;
-      cif0.iREN = 0;
-      cif0.dREN = 0;
-      cif0.dWEN = 0;
-      cif0.dstore = 0;
-      cif0.daddr = 0;
-      #(10)
-      cif0.iaddr = 0;
-      cif0.iREN = 0;
-      cif0.dREN = 0;
-      cif0.dWEN = 1;
-      cif0.dstore = 233;
-      cif0.daddr = 100;
-      if(cif0.dwait == 1)
-        $display("W started\n");
-      else
-        $display("gg start\n");
-          #(3)
-      if(cif0.dwait == 0)
-        $display("W done\n");
-      else
-        $display("gg done\n");
-//222222222222222222222
-      cif0.iaddr = 0;
-      cif0.iREN = 0;
-      cif0.dREN = 1;
-      cif0.dWEN = 0;
-      cif0.dstore = 233;
-      cif0.daddr = 100;
-      #(10)
-      if(ccif.ramload == 233)
-        $display("yup\n");
-      else
-        $display("no!! %d", ccif.ramload);
-      #10
-      dump_memory();
+  typedef enum { iREAD, dREAD, dWRITE } cstate;
+  static integer ecnt = 0;
+
+  initial begin
+    // Test Reset
+    reset();
+    dRead(0);
+    if (cif.dload != 32'h340100F0) error("Reset Failed.", ecnt);
+
+    // Test Write -> Read
+    dWrite(0, 99);
+    dRead(0);
+    if (cif.dload != 99) error("Write Failed.", ecnt);
+    #PERIOD;
+    dRead(10);
+    #(PERIOD*4);
+    if (cif.dload == 99) error("Write wrong location.", ecnt);
+    $display("TOTAL ERRORS: %0d", ecnt);
+    dump_memory();
     $finish;
- end  
-    
- task automatic dump_memory();   
- string filename = "memcpu.hex";
+  end
+
+  // Reset RAM
+  task reset;
+    nRST = 1'b1; #PERIOD;
+    nRST = 1'b0; #PERIOD;
+    nRST = 1'b1; #PERIOD;
+  endtask
+
+  task set(cstate state);
+    cif.iREN = state == iREAD;
+    cif.dREN = state == dREAD;
+    cif.dWEN = state == dWRITE;
+    #PERIOD;
+  endtask
+
+  task dWrite(word_t addr, word_t value);
+    cif.daddr = addr; cif.dstore = value; set(dWRITE);
+  endtask
+
+  task dRead(word_t addr);
+    cif.daddr = addr; set(dREAD);
+  endtask
+
+  task iRead(word_t addr, word_t value);
+    cif.iaddr = addr; set(iREAD);
+  endtask
+
+  task automatic dump_memory();
+    string filename = "memcpu.hex";
     int memfd;
 
-    cif0.daddr = 0;
-    cif0.dWEN = 0;
-    cif0.dREN = 0;
+    cif.daddr = 0;
+    cif.iREN = 0;
+    cif.dWEN = 0;
+    cif.dREN = 0;
 
     memfd = $fopen(filename,"w");
     if (memfd)
@@ -103,30 +91,27 @@ module memory_control_tb;
       bit [7:0][7:0] values;
       string ihex;
 
-      cif0.daddr = i << 2;
-      cif0.dREN = 1;
+      cif.daddr = i << 2;
+      cif.dREN = 1;
       repeat (4) @(posedge CLK);
-      if (cif0.dload === 0)
+      if (cif.dload === 0)
         continue;
-      values = {8'h04,16'(i),8'h00,cif0.dload};
-      foreach (values[j])
-        chksum += values[j];
+      values = {8'h04,16'(i),8'h00,cif.dload};
+      foreach (values[j]) chksum += values[j];
       chksum = 16'h100 - chksum;
-      ihex = $sformatf(":04%h00%h%h",16'(i),cif0.dload,8'(chksum));
+      ihex = $sformatf(":04%h00%h%h",16'(i),cif.dload,8'(chksum));
       $fdisplay(memfd,"%s",ihex.toupper());
     end //for
     if (memfd)
     begin
-      cif0.dREN = 0;
+      cif.dREN = 0;
       $fdisplay(memfd,":00000001FF");
       $fclose(memfd);
       $display("Finished memory dump.");
     end
   endtask
-    
 
-
-
-  
-
+  task automatic error(string message, ref integer e);
+    $display("ERROR: %s | dload - %0d", message, cif.dload); e++;
+  endtask
 endmodule
