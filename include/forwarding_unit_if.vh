e12d614d7a98ce02ed93687fa469cfdb3cd52e1a
Pradyuman Vig <pradyumanvig@outlook.com>
2016-09-30 05:38:02 -0400

    add forwarding unit

--- include/forwarding_unit_if.vh
+++ include/forwarding_unit_if.vh
@@ -4,20 +4,24 @@
 `include "cpu_types_pkg.vh"
 `include "mux_types_pkg.vh"
 
-import cpu_types_pkg::word_t;
+import cpu_types_pkg::*;
 import mux_types_pkg::fwdMux;
 
 interface forwarding_unit_if;
-  logic [25:11] ex_reg, mem_reg, wb_reg;
+  logic [25:16] ex_reg, mem_reg, wb_reg;
   logic ex_rfWEN, mem_rfWEN, wb_rfWEN;
-  word_t aluout, alubr_f;
+  regbits_t ex_dest, mem_dest, wb_dest;
+  word_t ex_aluout, mem_aluout, wb_aluout;
+  word_t rdat1_f, rdat2_f, alubr_f;
   fwdMux brSel_f, aSel_f, bSel_f;
 
   modport fu (
     input  ex_reg, mem_reg, wb_reg,
+           ex_dest, mem_dest, wb_dest,
            ex_rfWEN, mem_rfWEN, wb_rfWEN,
-           aluout,
-    output alubr_f, brSel_f, aSel_f, bSel_f
+           ex_aluout, mem_aluout, wb_aluout,
+    output brSel_f, aSel_f, bSel_f,
+           rdat1_f, rdat2_f, alubr_f
   );
 
 endinterface
--- source/forwarding_unit.sv
+++ source/forwarding_unit.sv
@@ -1,4 +1,29 @@
 `include "forwarding_unit_if.vh"
 
 module forwarding_unit(forwarding_unit_if.fu fuif);
+  assign ex_rs = fuif.ex_reg[25:21];
+  always_comb begin
+    fuif.aSel_f = 0;
+    if (fuif.mem_rfWEN && fuif.mem_dest != 0 && fuif.mem_dest == ex_rs) begin
+      fuif.aSel_f = 1;
+      fuif.rdat1_f = fuif.mem_aluout;
+    end
+    else if (fuif.wb_rfWEN && fuif.wb_dest != 0 && fuif.wb_dest == ex_rs) begin
+      fuif.aSel_f = 1;
+      fuif.rdat1_f = fuif.wb_aluout;
+    end
+  end
+
+  assign ex_rt = fuif.ex_reg[20:16];
+  always_comb begin
+    fuif.bSel_f = 0;
+    if (fuif.mem_rfWEN && fuif.mem_dest != 0 && fuif.mem_dest == ex_rt) begin
+      fuif.bSel_f = 1;
+      fuif.rdat2_f = fuif.mem_aluout;
+    end
+    else if (fuif.wb_rfWEN && fuif.wb_dest != 0 && fuif.wb_dest == ex_rt) begin
+      fuif.bSel_f = 1;
+      fuif.rdat2_f = fuif.wb_aluout;
+    end
+  end
 endmodule
